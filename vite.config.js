cc07de0160c049ea7dc0fd2c703a2dd3c37fd185
homeqwert <home1201@gmail.com>
2024-06-23 20:27:12 +0900

    gh-pages update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from "node:url";
+import { basename } from "node:path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -12,6 +13,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: `/photobook345kv/`,
   css: {
     postcss: {
       plugins: [],
